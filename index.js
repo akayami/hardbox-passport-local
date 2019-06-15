268d62508c80fe62836b051f079a6542b4666f86
Tomasz Rakowski <akayami@gmail.com>
2019-06-15 14:32:03 -0400

    Fixed things up and added tests

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
 const morgan = require('morgan');
 const express = require('express');
 const session = require('express-session');
-const ejs = require('ejs');
+//const ejs = require('ejs');
 const passport = require('passport');
 const LocalStrategy = require('passport-local-generic').Strategy;
 const HttpForbidden = require('./lib/error/http/forbidden');
 const bodyParser = require("body-parser");
+const app = express();
 
 module.exports = function (config) {
 
-
-	passport.serializeUser(function (user, done) {
-		done(null, user);
-	});
-
-	passport.deserializeUser(function (obj, done) {
-		if (!config.allowedDomains || config.allowedDomains.includes(obj.domain)) {
-			done(null, obj);
-		} else {
-			done(new HttpForbidden('Domain mismatch: ' + obj.domain + ' not in ' + config.allowedDomains))
-		}
-	});
+	//return function (req, res, cb) {
 
 	passport.use(new LocalStrategy({
 			fields: ['customer', 'email', 'password']
 		},
 		function (cred, done) {
-			config.authenticate(cred, (err, result, message) => {
+			config.authenticate(cred, (err, result) => {
 				if (err) {
-					return done(null)
+					return done(err)
 				} else if (result) {
 					return done(null, result);
 				} else {
-					return done(null, false, message);
+					return done(null, false, {message: 'Incorrect Login'});
 				}
 			});
 		}
 	));
 
-	const app = express();
+	passport.serializeUser(function (user, done) {
+		console.debug('Serialize');
+		done(null, user);
+	});
 
-	app.set('view engine', 'ejs');
-	app.use(morgan(config.morgan.format, config.morgan.options));
+	passport.deserializeUser(function (obj, done) {
+		console.debug('Deserialize');
+		done(null, obj);
+		// if (!config.allowedDomains || config.allowedDomains.includes(obj.domain)) {
+		// 	done(null, obj);
+		// } else {
+		// 	done(new HttpForbidden('Domain mismatch: ' + obj.domain + ' not in ' + config.allowedDomains))
+		// }
+	});
+
+	app.set('trust proxy', 1);
+	// app.use(require('cookie-parser')());
+	// app.set('view engine', 'ejs');
+	if (config.morgan) {
+		app.use(morgan(config.morgan.format, config.morgan.options));
+	}
 	app.use(config.local.login.loginURL, bodyParser.urlencoded({extended: false}));
 
 
 	// This needs to be initialized before session (fugly)
 	if (config.session.storeConf) {
+		console.debug('Initializing custom store');
 		const sessionStore = require(config.session.storeConf.type)(session);
 		config.session.store = new sessionStore(config.session.storeConf.config);
 	}
@@ -60,42 +68,75 @@ module.exports = function (config) {
 	app.use(config.local.login.loginURL, passport.initialize());
 	app.use(config.local.login.loginURL, passport.session());
 
+	// app.use(config.secureNamespace, (req, res, next) => {
+	// 	console.debug('Secure Passport Initialize');
+	// 	passport.initialize()(req, res, );
+	// 	passport.session()(req, res);
+	// 	next();
+	// });
+	//
+	// app.use(config.local.login.loginURL, (req, res, next) => {
+	// 	console.debug('Login Passport Initialize');
+	// 	passport.initialize()(req, res);
+	// 	console.debug('* Login Passport Initialize')
+	// 	passport.session()(req, res);
+	// 	console.debug('*** Login Passport Initialize')
+	// 	next();
+	// });
 
-	return function (req, res, cb) {
 
-		if (config.forwardLogin === false) {
-			app.get(config.loginURL, function (req, res, next) {
-				req.internalURL = true;
-				res.render('login');
-			})
-		}
+	app.use((req, res, next) => {
+		req.session.views = (req.session.views || 0) + 1;
+		res.append('session-view-count', req.session.views);
+		next();
+	});
 
-		app.post(
-			config.local.login.loginURL,
-			passport.authenticate(
-				'local-generic',
-				config.passport.authenticate(req, res)
-			)
-		);
 
-		app.get(config.logoutURL, function (req, res, next) {
+	//return function (req, res, cb) {
+
+	if (config.forwardLogin === false) {
+		app.get(config.loginURL, function (req, res, next) {
 			req.internalURL = true;
-			req.session.destroy();
-			req.logout();
-			res.redirect(config.loginURL);
-		});
+			res.render('login');
+		})
+	}
 
-		app.use(config.secureNamespace, function (req, res, next) {
-			if (!req.user) {
-				if (config.allowUnauthorized !== true && req.internalURL !== true) {
-					res.redirect(config.loginURL);
-				} else {
-					next();
-				}
+
+	app.post(config.local.login.loginURL, passport.authenticate('local-generic', config.passport.authenticate));
+
+	// app.post(
+	// 	config.local.login.loginURL, (req, res) => {
+	// 		console.debug('Running Passport Authenticate');
+	// 		passport.authenticate('local-generic', config.passport.authenticate)(req, res);
+	// 	}
+	// );
+
+	app.get(config.logoutURL, function (req, res, next) {
+		console.debug('Logout Handler');
+		req.internalURL = true;
+		req.session.destroy();
+		req.logout();
+		res.redirect(config.loginURL);
+	});
+
+	app.use(config.secureNamespace, function (req, res, next) {
+		if (!req.user) {
+			console.debug('No User Session');
+			console.debug('Unauthorized allowed: ', !config.allowUnauthorized !== true);
+			console.debug('Internal URL: ', !req.internalURL !== true);
+			if (config.allowUnauthorized !== true && req.internalURL !== true) {
+				console.debug('Redirecting...' + config.loginURL);
+				res.redirect(config.loginURL);
 			} else {
+				console.debug('Allow unauthorized access on - Letting through');
 				next();
 			}
-		});
+		} else {
+			next();
+		}
+	});
+
+	return function (req, res, cb) {
 
 		app.use(function (err, req, res, next) {
 			if (err instanceof HttpForbidden) {
@@ -106,8 +147,10 @@ module.exports = function (config) {
 		});
 
 		app.use(function (req, res, next) {
+			console.log('here');
 			if (req.user) {
-				res.proxyHeaders.push([config.headerName, JSON.stringify(req.user)]);
+				res.setHeader(config.headerName, JSON.stringify(req.user));
+				//res.proxyHeaders.push([config.headerName, JSON.stringify(req.user)]);
 			}
 			cb(null, req, res);
 		});
---
+++ test/bdd.test.js
@@ -0,0 +1,207 @@
+const request = require('request');
+const http = require('http');
+const async = require('async');
+const {expect} = require('chai');
+let ser1, handler, port = 18081;
+
+
+describe('BDD Tests', () => {
+	
+	const login_path = '/login';
+
+	const config = {
+		local: {
+			login: {
+				loginURL: login_path // This is where authentication is posted using POST
+			}
+		},
+		session: {
+			secret: 'keyboard cat',
+			resave: false,
+			saveUninitialized: true,
+			//cookie: {secure: true}
+		},
+		secureNamespace: '/secure', 		// This is the secured namespace for which login is checked
+		passport: {
+			authenticate: {
+				failureRedirect: '/login'	// This is where user is redirected when login fails
+			}
+		},
+		authenticate: (cred, cb) => {			// This is a plugable authentication function
+			if (cred.customer === 'fail') {
+				cb(null, false, {message: 'Incorrect Login'})
+			} else {
+				cb(null, {profile: 'Some User'})
+			}
+		},
+		logoutURL: '/logout',			// Triggers logout sequence
+		loginURL: '/loginURL',			// Provides Login inteface
+		headerName: 'hdx-user'			// header name
+	}
+	
+	beforeEach((done) => {
+		let h = require('../index')(config);
+		
+		let handler = (req, res) => {
+			h(req, res, (err, req, res) => {
+				res.end();
+			});
+		};
+		
+		ser1 = require('http').createServer(handler).listen(port, (err) => {
+			if (err) return done(err);
+			done();
+		})
+	});
+	
+	afterEach(() => {
+		if (ser1) ser1.close();
+	});
+	
+	
+	it('Login Success', (done) => {
+		async.series([
+			(cb) => {
+				require('request')({
+					url: `http://localhost:${port}${login_path}`,
+					method: 'POST',
+					form: {
+						customer: 'user',
+						email: 'email',
+						password: 'password'
+					}
+				}, (err, res, body) => {
+					expect(res.statusCode).equal(200);
+					//expect(res.headers.location).equal(login_path);
+					return cb(err);
+				});
+			}
+		], (err, res) => {
+			if (err) return done(err);
+			done();
+		});
+	});
+	
+	it('Login Failed', (done) => {
+		async.series([
+			(cb) => {
+				require('request')({
+					url: `http://localhost:${port}${login_path}`,
+					method: 'POST',
+					form: {
+						customer: 'fail',
+						email: 'email',
+						password: 'password'
+					}
+				}, (err, res, body) => {
+					expect(res.statusCode).equal(302);
+					expect(res.headers.location).equal(login_path);
+					return cb(err);
+				});
+			}
+		], (err, res) => {
+			if (err) return done(err);
+			done();
+		});
+	});
+	
+	it('No Login', (done) => {
+		async.series([
+			(cb) => {
+				require('request')({
+					url: `http://localhost:${port}/secure`,
+					method: 'GET',
+					followRedirect: false
+				}, (err, res, body) => {
+					expect(res.statusCode).equal(302);
+					return cb(err);
+				});
+			}
+		], (err, res) => {
+			if (err) return done(err);
+			done();
+		});
+	});
+	
+	it('Access Secure When Authorized', (done) => {
+		const j = require('request').jar();
+		const request = require('request').defaults({jar: j});
+		async.series([
+			(cb) => {
+				request({
+					url: `http://localhost:${port}${login_path}`,
+					method: 'POST',
+					form: {
+						customer: 'user',
+						email: 'email',
+						password: 'password'
+					}
+				}, (err, res, body) => {
+					expect(res.statusCode).equal(200);
+					//expect(res.headers.location).equal(login_path);
+					return cb(err);
+				});
+			},
+			(cb) => {
+				request({
+					url: `http://localhost:${port}/secure`,
+					method: 'GET',
+					followRedirect: false,
+					headers: {
+						Cookie: j.getCookieString(`http://localhost:${port}`)
+					}
+				}, (err, res, body) => {
+					expect(res.statusCode).equal(200);
+					return cb(err);
+				});
+			}
+		], (err, res) => {
+			if (err) return done(err);
+			done();
+		});
+	});
+	
+	it('Login Failed Full Chain', (done) => {
+		async.series([
+			(cb) => {
+				require('request')({
+					url: `http://localhost:${port}${config.local.login.loginURL}`,
+					method: 'POST',
+					form: {
+						customer: 'fail',
+						email: 'email',
+						password: 'password'
+					}
+				}, (err, res, body) => {
+					expect(res.statusCode).equal(302);
+					expect(res.headers.location).equal(login_path);
+					return cb(err);
+				});
+			},
+			(cb) => {
+				require('request')({
+					url: `http://localhost:${port}${config.secureNamespace}`,
+					method: 'GET',
+					followRedirect: false
+				}, (err, res, body) => {
+					expect(res.statusCode).equal(302);
+					return cb(err);
+				});
+			},
+			(cb) => {
+				require('request')({
+					url: `http://localhost:${port}${config.logoutURL}`,
+					method: 'GET',
+					followRedirect: false
+				}, (err, res, body) => {
+					expect(res.statusCode).equal(302);
+					expect(res.headers.location).equal(config.loginURL);
+					return cb(err);
+				});
+			}
+		], (err, res) => {
+			if (err) return done(err);
+			done();
+		});
+	});
+});
